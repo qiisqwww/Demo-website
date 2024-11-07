5020589366ce722ee4bc5d477a5b7ea06c1719d4
qiisqwww <qiisqwww@gmail.com>
2024-11-08 00:29:33 +0300

    pushed branch to last master commit

---
+++ api/src/presentation/routers/token_router.py
@@ -0,0 +1,37 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, status, HTTPException
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from jwt import InvalidTokenError
+
+from src.get_service import get_auth_service
+from src.services import AuthService, UserWasNotFoundException
+
+__all__ = [
+    "token_router"
+]
+
+
+token_router = APIRouter()
+http_bearer = HTTPBearer()
+
+
+@token_router.get("/token")
+async def validate_token(
+        token: Annotated[HTTPAuthorizationCredentials, Depends(http_bearer)],
+        auth_service: AuthService = Depends(get_auth_service)
+):
+    try:
+        await auth_service.authorize(token.credentials)
+    except InvalidTokenError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate token",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    except UserWasNotFoundException:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
