8f24282bfebce6198c45834813b32f728aeef516
Balex <morevbalex777@gmail.com>
2024-11-12 23:22:00 +0300

    feat: add profile loading

--- client/src/pages/Profile/Profile.tsx
+++ client/src/pages/Profile/Profile.tsx
@@ -5,8 +5,8 @@ import { useEffect, useState } from "react"
 import Cookies from "js-cookie"
 import styles from "./Profile.module.css"
 import { Navigate } from "react-router-dom"
-import { Avatar, Card } from "antd"
-import { LogoutOutlined } from "@ant-design/icons"
+import { Avatar } from "antd"
+import { LoadingOutlined } from "@ant-design/icons"
 
 interface IProfileData{
 	username: string
@@ -21,7 +21,7 @@ export default function Profile() {
 		email: "",
 		birthdate: ""
 	})
-	const [loading, setLoading] = useState(false)
+	const [loading, setLoading] = useState(true)
 
 	const token = Cookies.get('token');
 
@@ -34,9 +34,9 @@ export default function Profile() {
 
 	const fetchData = async () => {
 		try{
+			setLoading(true)
 			const response = await axiosInstance.get<IProfileData>(`${import.meta.env.VITE_API_URL}/me`)
 			setUser(response.data)
-			setLoading(true)
 		}catch(e:unknown){
 			logout()
 			console.error(e)
@@ -56,21 +56,27 @@ export default function Profile() {
 
 	if (isLogged){
 		return (
-			<>
-			<h1>Profile card</h1>
-			<Card loading={loading} actions={[<LogoutOutlined onClick={logout} key="edit" />]} style={{ minWidth: 300 }}>
-        <Card.Meta
-          avatar={<Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=1" />}
-          title={user.username}
-          description={
-            <>
-              <p className={styles.text}>Birthday:  <span className={styles.data}>{user.birthdate}</span></p>
-							<p className={styles.text}>Email:  <span className={styles.data}>{user.email}</span></p>
-            </>
-          }
-        />
-      </Card>
-			</>
+			<div className={styles.page}>
+				<h1 className={styles.title}>Profile</h1>
+				<hr />
+				{loading ?
+					<LoadingOutlined style={{position: "absolute", left: "50%", fontSize: 30}}/>
+					:
+					<div className={styles.flex}>
+						<Avatar className={styles.avatar} src="https://api.dicebear.com/7.x/miniavs/svg?seed=1" />
+						<div>
+							<h2 className={styles.username}>{user.username}</h2>
+							<div className={styles.flex}>
+								<h3 className={styles.email}>{user.email}</h3>
+								<span className={styles.birth}>({user.birthdate})</span>
+							</div>
+						</div>
+						<button className={styles.button} onClick={logout}>
+							Log out
+						</button>
+					</div>
+				}
+			</div>
 		)
 	}else{
 		return <Navigate to="/login"/>
