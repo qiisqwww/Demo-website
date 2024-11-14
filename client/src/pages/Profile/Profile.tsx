9b22ac996ceb4c77bd2482e8efbbf4682152ac17
Balex <morevbalex777@gmail.com>
2024-11-15 00:42:37 +0300

    feat: add photo editor

--- client/src/pages/Profile/Profile.tsx
+++ client/src/pages/Profile/Profile.tsx
@@ -4,11 +4,12 @@ import { useEffect, useState } from "react"
 import Cookies from "js-cookie"
 import styles from "./Profile.module.css"
 import { Navigate } from "react-router-dom"
-import { message, Modal, Upload, UploadProps } from "antd"
-import { ExclamationCircleOutlined, LoadingOutlined } from "@ant-design/icons"
+import { message, Modal, Upload } from "antd"
+import { EditOutlined, ExclamationCircleOutlined, LoadingOutlined } from "@ant-design/icons"
 import dayjs from 'dayjs';
 import CustomModal from "../../components/CustomModal/CustomModal"
 import { RcFile } from "antd/es/upload"
+import ImgCrop from 'antd-img-crop';
 
 interface IProfileData{
 	username: string
@@ -29,22 +30,6 @@ export default function Profile() {
 	})
 	const [loading, setLoading] = useState(true)
 
-	const handleChange: UploadProps['onChange'] = (info) => {
-		console.log(info)
-    if (info.file.status === 'uploading') {
-      setLoading(true);
-      return;
-    }
-    if (info.file.status === 'done') {
-			setLoading(false);
-			return
-    }
-		if (info.file.status === 'error'){
-			setLoading(false);
-			return
-		}
-  };
-
 	const changePhoto = async (file: RcFile): Promise<void> => {
 		try{
 			setLoading(true)
@@ -52,13 +37,14 @@ export default function Profile() {
     	formData.append('user_image', file);
 			const response = await axiosInstance.post(`${import.meta.env.VITE_API_URL}/profile-image`, formData)
 			setUser(response.data)
-			setLoading(false)
 		}catch(error: unknown){
 			const e = error as AxiosError;
 			if (e.request.status === 422){
-				const message = JSON.parse(e.request.response)
-				console.log(message)
+				const m = JSON.parse(e.request.response)
+				message.error(m.detail);
 			}
+		}finally{
+			setLoading(false)
 		}
 	}
 
@@ -71,7 +57,6 @@ export default function Profile() {
 		if (!isLt2M) {
 			message.error('Image must smaller than 10MB!');
 		}
-	
 		if (isJpgOrPng && isLt2M){
 			changePhoto(file)
 		}
@@ -133,16 +118,20 @@ export default function Profile() {
 						<LoadingOutlined style={{position: "absolute", left: "50%", fontSize: 30, marginTop: 20}}/>
 						:
 						<div className={styles.flex}>
+							<ImgCrop rotationSlider>
 							<Upload
 								name="avatar"
-								listType="picture-circle"
+								listType="picture"
 								className={styles.avatarUploader}
 								showUploadList={false}
 								beforeUpload={beforeUpload}
-								onChange={handleChange}
 							>
 								<img src={import.meta.env.VITE_STATIC_URL + user.photo_url} alt="avatar" style={{ width: '100%' }} className={styles.avatar}/>
+								<div className={styles.editAvatar}>
+									<EditOutlined className={styles.editAvatarIcon}/>
+								</div>
 							</Upload>
+							</ImgCrop>
 							<div>
 								<h2 className={styles.username}>{user.username}</h2>
 								<div className={styles.flex}>
